748dae5967bd16a5da47e55fa37eb649a72fd734
Giordani <dani.santossilveira@hotmail.com>
2024-04-22 18:41:54 -0300

    ajustes models visitacoes

--- app/Models/Enfermo.php
+++ app/Models/Enfermo.php
@@ -9,37 +9,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Enfermo extends BaseModel
 {
-    use HasFactory;
-
-    protected $fillable = ['id_usuario', 'nome', 'created_at'];
-
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
         ->logFillable()
         ->logOnly(['user.name'])
         ->useLogName('Visita Enfermo');
-    }
-
-
-    public static function rules(): array
-    {
-        return [
-            'id_usuario' => 'exists:users,id',
-            'nome' => 'max:150',
-        ];
-    }
-
-    public static function feedback(): array
-    {
-        return [
-            'id_usuario.exists' => 'O usuário informado não existe!',
-            'nome.max' => 'O campo :attribute não pode ultrapassar 150 caracteres.',
-        ];
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo('App\Models\User', 'id_usuario', 'id');
     }
 }
--- app/Models/Presidio.php
+++ app/Models/Presidio.php
@@ -2,43 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Presidio extends BaseModel
+class Presidio extends Visita
 {
-    use HasFactory;
-
-    protected $fillable = ['id_usuario', 'nome', 'created_at'];
-
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
         ->logFillable()
         ->logOnly(['user.name'])
         ->useLogName('Visita Presidio');
-    }
-
-    public static function rules(): array
-    {
-        return [
-            'id_usuario' => 'exists:users,id',
-            'nome' => 'max:150',
-        ];
-    }
-
-    public static function feedback(): array
-    {
-        return [
-            'id_usuario.exists' => 'O usuário informado não existe!',
-            'nome.max' => 'O campo :attribute não pode ultrapassar 150 caracteres.',
-        ];
-    }
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo('App\Models\User', 'id_usuario', 'id');
     }
 }
